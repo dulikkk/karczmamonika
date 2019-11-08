cfb3edc898d66ca3cc3575e1063762c999ce901c
kub4k1 <kub4k1@gmail.com>
2019-11-08 16:46:44 +0100

    Finish computer view
    I finished computer view and I added comment slider

--- js/script.js
+++ js/script.js
@@ -1,18 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const nav = document.querySelector('.menu');
     const links = document.querySelectorAll('.menu-list-anchor');
-    console.log(links)
+    function scrollInto() {
+        console.log(links)
 
-    for (const a of links) {
-        a.addEventListener('click', function (e) {
-            e.preventDefault();
-            const href = a.getAttribute('href');
-            const target = document.querySelector(href);
+        for (const a of links) {
+            a.addEventListener('click', function (e) {
+                e.preventDefault();
+                const href = a.getAttribute('href');
+                const target = document.querySelector(href);
 
-            target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
+                target.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
             });
-        });
 
+        }
     }
-})
+
+    function fixedMenu() {
+
+        if (document.body.scrollTop > 950 || document.documentElement.scrollTop > 950) {
+            nav.classList.remove('menu');
+            nav.classList.add('fixed_menu');
+            for (const a of links) {
+                a.style.color = "#000";
+                a.style.textShadow = "none";
+            }
+        }
+        else {
+            nav.classList.remove('fixed_menu');
+            nav.classList.add('menu');
+            for (const a of links) {
+                a.style.color = "#fff";
+                a.style.textShadow = "black 0.1em 0.1em 0.2em";
+            }
+        }
+    }
+
+    scrollInto();
+    window.onscroll = function () { fixedMenu() };
+});
